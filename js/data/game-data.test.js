85096797e872ac48dcebae8dd991987972800feb
Vladimir Korshunov <bekobou@yandex.ru>
2016-12-19 04:10:10 +0300

    Удали тест для примера

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -2,14 +2,6 @@ import assert from 'assert';
 
 import {setLifes, setTimer, initialGame} from './game-data.js';
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
-    });
-  });
-});
-
 describe('Game Data', function () {
   describe('setLifes', function () {
     it('Попытка присвоить отрицательное количество жизней', function () {
