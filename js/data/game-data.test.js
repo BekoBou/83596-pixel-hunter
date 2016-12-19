2505509ac2b85144d44cd0b3923707780c97f53d
Vladimir Korshunov <bekobou@yandex.ru>
2016-12-19 04:34:34 +0300

    Измени описания тестов

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,16 +1,15 @@
 import assert from 'assert';
-
 import {setLifes, setTimer, initialGame} from './game-data.js';
 
 describe('Game Data', function () {
   describe('setLifes', function () {
-    it('Попытка присвоить отрицательное количество жизней', function () {
+    it('должен кидать исключение, если передано отрицательное значение жизней', function () {
       assert.throws(() => setLifes(initialGame, -1));
     });
-    it('Попытка присвоить больше максимального количества жизней', function () {
+    it('должен кидать исключение, если передано значение больше максимального значения жизней', function () {
       assert.throws(() => setLifes(initialGame, 4));
     });
-    it('Присвоение корректного количества жизней', function () {
+    it('должен менять значение количества жизней в состоянии игры', function () {
       let game = setLifes(initialGame, 1);
       let testGame = Object.assign({}, initialGame, {lifes: 1});
 
@@ -19,13 +18,13 @@ describe('Game Data', function () {
   });
 
   describe('setTimer', function () {
-    it('Попытка присвоить отрицательное значение таймера', function () {
+    it('должен кидать исключение, если передано отрицательное значение таймера', function () {
       assert.throws(() => setTimer(initialGame, -1));
     });
-    it('Попытка присвоить больше максимального значения таймера', function () {
+    it('должен кидать исключение, если передано больше максимального значения таймера', function () {
       assert.throws(() => setTimer(initialGame, 31));
     });
-    it('Присвоение корректного значения таймера', function () {
+    it('должен изменять значение таймера в состоянии игры', function () {
       let game = setTimer(initialGame, 25);
 
       assert.strictEqual(game.timer, 25);
